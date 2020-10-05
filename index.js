34baf8ed579b4f43bad7a59f8a1df77acceb7029
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-05 16:32:47 -0300

    passando a rota dos views

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'database.sqlite'), { P
 
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))

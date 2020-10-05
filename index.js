f594a8f469ed5e25c430c4ac844d2082373ee926
Stefano Paulo <stefanopaulo47@gmail.com>
2020-10-05 09:56:22 -0300

    instalando e testando o sqlite

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const { render } = require('ejs')
 const express = require('express')
 const app = express()
 
+const sqlite = require('sqlite')
+const dbConnection = sqlite.open('database.sqlite', { Promise })
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const db = await dbConnection
+    const categories = await db.all('select * from categories')
+    
+    res.render('home', {
+        categories
+    })
 })
 
 app.get('/vaga', (req, res) => {
     res.render('vaga')
 })
+
+
+const init = async () => {
+    const db = await dbConnection
+    await db.run('create table if not exists categories(id INTEGER PRIMARY KEY, category TEXT)')
+    // const category = 'Marketing team'
+    // await db.run(`insert into categories(category) values('${category}')`)
+}
+init()
 
 app.listen(3000, err => {
     if(err) {
